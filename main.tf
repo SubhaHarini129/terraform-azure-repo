697dd6709c4a91660d3a087fd1b94aab7b645017
“SubhaHarini129” <subhaharini129@gmail.com>
2025-09-17 00:53:12 +0530

    2VMs and load balancer for high availability

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0" # latest major version
+      version = "~> 4.0"
     }
   }
 
   required_version = ">= 1.3.0"
 
-   backend "azurerm" {
+  backend "azurerm" {
     resource_group_name  = "learn-terraform-rg"
     storage_account_name = "harinilearnterraform123"
     container_name       = "tfstate"
@@ -16,21 +16,17 @@ terraform {
   }
 }
 
-provider azurerm {
-    features {}
-    subscription_id = "320fb61f-2500-4465-9cb2-ca21d99aa5b2"
+provider "azurerm" {
+  features {}
+  subscription_id = "320fb61f-2500-4465-9cb2-ca21d99aa5b2"
 }
 
-# resource "azurerm_resource_group" "rg" {
-#   name     = "learn-terraform-rg"
-#   location = "East US"
-# }
-
-
+# ---------- Resource Group & Storage ----------
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.location
 }
+
 resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
@@ -39,11 +35,7 @@ resource "azurerm_storage_account" "storage" {
   account_replication_type = var.storage_account_replication
 }
 
-# CLI command to create tfstate container for the storage account
-# az storage container create \
-#   --name tfstate \
-#   --account-name harinilearnterraform123
-
+# ---------- Networking ----------
 resource "azurerm_virtual_network" "vnet" {
   name                = "learn-terraform-vnet"
   location            = azurerm_resource_group.rg.location
@@ -58,26 +50,41 @@ resource "azurerm_subnet" "subnet1" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Create a Network Security Group
+# ---------- Network Security Group & Rules ----------
 resource "azurerm_network_security_group" "nsg" {
   name                = "learn-terraform-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-# Add a rule to allow SSH (port 22)
+# Allow SSH (priority 100)
 resource "azurerm_network_security_rule" "ssh_rule" {
   name                        = "SSH"
-  priority                    = 1001
+  priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefix       = "*"
+  source_address_prefix       = "*"    # tighten this in production (e.g. your IP)
   destination_address_prefix  = "*"
   network_security_group_name = azurerm_network_security_group.nsg.name
   resource_group_name         = azurerm_resource_group.rg.name
+}
+
+# Allow HTTP (priority 200)
+resource "azurerm_network_security_rule" "allow_http" {
+  name                        = "Allow-HTTP"
+  priority                    = 200
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
 }
 
 # Associate NSG with Subnet
@@ -86,7 +93,7 @@ resource "azurerm_subnet_network_security_group_association" "subnet_assoc" {
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-# Public IP for the VM
+# ---------- VM1 Public IP, NIC, VM ----------
 resource "azurerm_public_ip" "vm_pip" {
   name                = "learn-terraform-vm-ip"
   location            = azurerm_resource_group.rg.location
@@ -95,7 +102,6 @@ resource "azurerm_public_ip" "vm_pip" {
   sku                 = "Standard"
 }
 
-# Network Interface
 resource "azurerm_network_interface" "vm_nic" {
   name                = "learn-terraform-nic"
   location            = azurerm_resource_group.rg.location
@@ -109,12 +115,11 @@ resource "azurerm_network_interface" "vm_nic" {
   }
 }
 
-# Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "learn-terraform-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s" # Small free-tier eligible size
+  size                = "Standard_B2s"
   admin_username      = "azureuser"
 
   network_interface_ids = [
@@ -133,9 +138,165 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 
-  # Use SSH key instead of password (more secure)
   admin_ssh_key {
     username   = "azureuser"
     public_key = file("/Users/subhaharini/.ssh/id_rsa.pub")
   }
 }
+
+# Use Custom Script Extension to install nginx on VM1 (reliable)
+resource "azurerm_virtual_machine_extension" "vm_nginx_ext" {
+  name                 = "install-nginx-vm1"
+  virtual_machine_id   = azurerm_linux_virtual_machine.vm.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.1"
+
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "apt-get update -y && apt-get install -y nginx && echo '<h1>Welcome from VM1</h1>' > /var/www/html/index.html && systemctl enable nginx && systemctl start nginx"
+    }
+  SETTINGS
+}
+
+# ---------- Managed Data Disk for VM1 ----------
+resource "azurerm_managed_disk" "vm_data_disk" {
+  name                 = "learn-terraform-datadisk"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 10
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "vm_data_disk_attach" {
+  managed_disk_id    = azurerm_managed_disk.vm_data_disk.id
+  virtual_machine_id = azurerm_linux_virtual_machine.vm.id
+  lun                = 0
+  caching            = "ReadWrite"
+}
+
+# ---------- Load Balancer ----------
+resource "azurerm_public_ip" "lb_pip" {
+  name                = "learn-terraform-lb-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_lb" "lb" {
+  name                = "learn-terraform-lb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.lb_pip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "bepool" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "BackendPool"
+}
+
+resource "azurerm_lb_probe" "http_probe" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "http-probe"
+  port            = 80
+  protocol        = "Tcp"
+}
+
+resource "azurerm_lb_rule" "http_rule" {
+  loadbalancer_id                = azurerm_lb.lb.id
+  name                           = "http-rule"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "PublicIPAddress"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bepool.id]
+  probe_id                       = azurerm_lb_probe.http_probe.id
+}
+
+# Associate VM1 NIC with LB backend pool
+resource "azurerm_network_interface_backend_address_pool_association" "nic_lb_assoc" {
+  network_interface_id    = azurerm_network_interface.vm_nic.id
+  ip_configuration_name   = "internal"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.bepool.id
+}
+
+# ---------- VM2 (second standalone VM) ----------
+resource "azurerm_public_ip" "vm2_pip" {
+  name                = "learn-terraform-vm2-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "vm2_nic" {
+  name                = "learn-terraform-vm2-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm2_pip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm2" {
+  name                = "learn-terraform-vm2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.vm2_nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("/Users/subhaharini/.ssh/id_rsa.pub")
+  }
+}
+
+# Use Custom Script Extension to install nginx on VM2 (reliable)
+resource "azurerm_virtual_machine_extension" "vm2_nginx_ext" {
+  name                 = "install-nginx-vm2"
+  virtual_machine_id   = azurerm_linux_virtual_machine.vm2.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.1"
+
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "apt-get update -y && apt-get install -y nginx && echo '<h1>Welcome from VM2</h1>' > /var/www/html/index.html && systemctl enable nginx && systemctl start nginx"
+    }
+  SETTINGS
+}
+
+# Associate VM2 NIC with LB backend pool
+resource "azurerm_network_interface_backend_address_pool_association" "vm2_lb_assoc" {
+  network_interface_id    = azurerm_network_interface.vm2_nic.id
+  ip_configuration_name   = "internal"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.bepool.id
+}
+
--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,15 @@ output "storage_account_name" {
 output "storage_account_primary_endpoint" {
   value = azurerm_storage_account.storage.primary_blob_endpoint
 }
+# ---------- Outputs ----------
+output "lb_public_ip" {
+  value = azurerm_public_ip.lb_pip.ip_address
+}
+
+output "vm1_public_ip" {
+  value = azurerm_public_ip.vm_pip.ip_address
+}
+
+output "vm2_public_ip" {
+  value = azurerm_public_ip.vm2_pip.ip_address
+}
